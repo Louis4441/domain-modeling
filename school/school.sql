0d70e0a1c7930464a50a03a583b9acbd467cf97e
Louis <louis.harel@kellogg.northwestern.edu>
2022-01-18 15:31:10 +0000

    New changes

--- school/school.sql
+++ school/school.sql
@@ -21,23 +21,24 @@ CREATE TABLE teachers (
   first_name TEXT,
   last_name TEXT,
   bio TEXT
-)
+);
 
 CREATE TABLE courses (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   description TEXT
-)
+);
 
 CREATE TABLE sections (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   time TEXT,
   course_id INTEGER,
   teacher_id INTEGER
-)
+);
 
 CREATE TABLE enrollments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  course_id INTEGER,
   section_id INTEGER,
-  student_id INTEGER,
-)
+  student_id INTEGER
+);
